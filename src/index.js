35c9ddf0de0775a512a9d63695ef54250fdf50f0
Yulia <juliam2007@mail.ru>
2021-04-15 17:25:52 +0300

    feat: add sorting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if (Array.isArray(matrix)) {
+        return matrix.map((el, index)=> Array.isArray(el) && (index - 1) % 2 === 0 ? el.sort((a,b) => b - a) : el.sort((a,b) => a - b)).flat();
+    }
+    if (Array.isArray(matrix) && matrix.lenght === 0) {
+        return [];
+    }
+    if (matrix === undefined) {
+        return [];
+    }
 }
